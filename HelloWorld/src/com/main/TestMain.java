4d4aeb6423c0a3ea067129c00ba1e3483dd9feb4
ketkib07 <ketkib07@gmail.com>
2023-08-04 17:51:18 +0530

    Changed the file

--- HelloWorld/src/com/main/TestMain.java
+++ HelloWorld/src/com/main/TestMain.java
@@ -5,7 +5,13 @@ public class TestMain {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
+		
+		System.out.println(print());
 
+	}
+	
+	public static String print(){
+		return "Hello World";
 	}
 
 	
